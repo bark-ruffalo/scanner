e913885ed55e12c4dcb260df099a02b0d49bc2a2
Bark Ruffalo <hype@trulyadog.com>
2025-03-27 21:06:03 +0100

    pnpm check:write

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,17 +4,17 @@ import { db } from "./db";
 import { launches } from "./db/schema";
 
 export async function getLaunches(filter?: string) {
-  return await db.query.launches.findMany({
-    where:
-      filter && filter !== "All" ? eq(launches.launchpad, filter) : undefined,
-    orderBy: (launches, { desc }) => [desc(launches.createdAt)],
-  });
+	return await db.query.launches.findMany({
+		where:
+			filter && filter !== "All" ? eq(launches.launchpad, filter) : undefined,
+		orderBy: (launches, { desc }) => [desc(launches.createdAt)],
+	});
 }
 
 export async function getDistinctLaunchpads() {
-  const distinctLaunchpads = await db
-    .selectDistinct({ launchpad: launches.launchpad })
-    .from(launches);
+	const distinctLaunchpads = await db
+		.selectDistinct({ launchpad: launches.launchpad })
+		.from(launches);
 
-  return distinctLaunchpads.map(({ launchpad }) => launchpad);
+	return distinctLaunchpads.map(({ launchpad }) => launchpad);
 }
